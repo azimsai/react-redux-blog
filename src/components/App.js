cf4fd356023e6db5312c49d9774e30719a2f3dfd
Azeem S <azeem@azeem-pc.localdomain>
2022-08-24 12:24:09 +0530

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import PostList from './PostList';
+import '../style/App.css';
+function App() {
+  return (
+    <div className="ui container App">
+      <PostList />
+    </div>
+  );
+}
+
+export default App;
